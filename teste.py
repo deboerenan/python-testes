53a0fb6ed00dc283f23b4b607ce5267376b463d5
Renan <renanqisilva@gmail.com>
2026-01-01 12:22:45 +0000

    Testes iniciais em Python

---
+++ teste.py
@@ -0,0 +1,6 @@
+nome1 = input('qual seu primeiro nome?')
+nome2 = input('qual é seu ultimo nome?')
+nome3 = input('qual é o nome da sua mãe?')
+estado = input('qual seu estado civil?')
+
+print (f'seu nome é {nome1} {nome2} filho da {nome3} e seu estado civil é {estado}. certo?') 
